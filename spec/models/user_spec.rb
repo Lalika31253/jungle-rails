60149a39f861783382d08ef18513fb62e2e391e6
Lalika31253 <Lalika31253@gmail.com>
2024-04-24 11:57:38 -0600

    Add authentication (class) method

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,11 +14,11 @@ RSpec.describe User, type: :model do
 
      #validates :first_name and :last_name
     it "first name, and last name should be required" do
-      @user = User.create(first_name: "", last_name: "Dylan", email: "RDylan@email.com", password: 'newpassword', password_confirmation: 'newpassword')
+      @user = User.create(first_name: "", last_name: "Dylan", email: "rdylan@email.com", password: 'newpassword', password_confirmation: 'newpassword')
       @user.save
       expect(@user.errors.full_messages).to include("First name can't be blank")
 
-      @user = User.create(first_name: "Rob", last_name: "", email: "RDylan@email.com", password: 'newpassword', password_confirmation: 'newpassword')
+      @user = User.create(first_name: "Rob", last_name: "", email: "rdylan@email.com", password: 'newpassword', password_confirmation: 'newpassword')
       @user.save
       expect(@user.errors.full_messages).to include("Last name can't be blank")
     end
@@ -32,11 +32,33 @@ RSpec.describe User, type: :model do
     end  
 
     it 'password should have a minimum 3 characters' do
-      @user = User.create(first_name: "Rob", last_name: "Dylan", email: "RDylan@email.com", password: '12', password_confirmation: '12')
+      @user = User.create(first_name: "Rob", last_name: "Dylan", email: "rdylan@email.com", password: '12', password_confirmation: '12')
       expect(@user.errors.full_messages).to include("Password is too short (minimum is 3 characters)")
     end
-
   end
+
+
+  describe '.authenticate_with_credentials' do
+  
+    it 'should log a user in with valid credentials' do
+      @user = User.create(first_name: "Rob", last_name: "Dylan", email: "rdylan@email.com", password: "something", password_confirmation: 'something')
+      @user_is_valid = User.authenticate_with_credentials("rdylan@email.com", "something")
+      expect(@user_is_valid).to_not equal(nil)
+    end
+
+    it 'should log user in with spaces in the email' do
+      @user = User.create(first_name: "Rob", last_name: "Dylan", email: "rdylan@email.com", password: "something", password_confirmation: 'something')
+      @user_is_valid = User.authenticate_with_credentials(" rdylan@email.com ", "something")
+      expect(@user_is_valid).to_not equal(nil)
+    end
+
+    it 'should log user in if email is in lower or upper case' do
+      @user = User.create(first_name: "Rob", last_name: "Dylan", email: "rdylan@email.com", password: "something", password_confirmation: 'something')
+      @user_is_valid = User.authenticate_with_credentials("RDylan@Email.com", "something")
+      expect(@user_is_valid).to_not equal(nil)
+    end
+  end
+
 end
 
     
