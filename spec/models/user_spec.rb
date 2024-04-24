0cbb3050c75be64d0c1d025e281771c4dacacbb1
Lalika31253 <Lalika31253@gmail.com>
2024-04-23 23:38:59 -0600

    Add test cases to check user authentication

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+
+  describe 'Validations' do
+
+     #validates :password and :password_confirmation
+    it 'is not valid when entered password and password confirmation does not match' do
+      @user = User.create(first_name: "Rob", last_name: "Dylan", email: "rdylan@email.com", password: 'newpassword', password_confirmation: 'somepassword')
+      @user.save
+      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+    end
+
+
+     #validates :first_name and :last_name
+    it "first name, and last name should be required" do
+      @user = User.create(first_name: "", last_name: "Dylan", email: "RDylan@email.com", password: 'newpassword', password_confirmation: 'newpassword')
+      @user.save
+      expect(@user.errors.full_messages).to include("First name can't be blank")
+
+      @user = User.create(first_name: "Rob", last_name: "", email: "RDylan@email.com", password: 'newpassword', password_confirmation: 'newpassword')
+      @user.save
+      expect(@user.errors.full_messages).to include("Last name can't be blank")
+    end
+
+
+    it 'is not valid when the email already exists in the DB' do
+      User.create(first_name: "Sam", last_name: "Smith", email: "smith@email.com", password: 'something', password_confirmation: 'something')
+      @user2 = User.new(first_name: "Rob", last_name: "Dylan", email: "smith@email.com", password: 'lhl2024', password_confirmation: 'lhl2024')
+      @user2.save
+      expect(@user2.errors.full_messages).to include("Email has already been taken")
+    end  
+
+    it 'password should have a minimum 3 characters' do
+      @user = User.create(first_name: "Rob", last_name: "Dylan", email: "RDylan@email.com", password: '12', password_confirmation: '12')
+      expect(@user.errors.full_messages).to include("Password is too short (minimum is 3 characters)")
+    end
+
+  end
+end
+
+    
+
